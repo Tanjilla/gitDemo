23e3a92fa6d9f6596078fa2c4e2c968b4dad99bb
Tanjilla <tanjillaislamqa@gmail.com>
2023-01-25 16:41:28 -0500

    updata

--- src/git_Hub_Demo/HelloGitHub.java
+++ src/git_Hub_Demo/HelloGitHub.java
@@ -2,7 +2,6 @@ package git_Hub_Demo;
 
 public class HelloGitHub {
 	public static void main(String[] args) {
-		System.out.println(" I know how to create a separate remote repository in Github and can push my project GithubDemo, without the help of anyone");
+		System.out.println(" I know how to create a separate remote repository in Github and can push my project GithubDemo, without the help of anyone"); 
 	}
-
 }
